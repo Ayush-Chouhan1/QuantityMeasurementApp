218506bcf64cbbbc851d89244c90cca36faa0301
Ayush-Chouhan1 <ayushraochouhan@gmail.com>
2026-03-17 14:34:36 +0530

    [Ayush]:added feature UC16 database integration

---
+++ src/main/java/com/app/quantitymeasurement/exception/QuantityMeasurementException.java
@@ -0,0 +1,7 @@
+package main.java.com.app.quantitymeasurement.exception;
+public class QuantityMeasurementException extends RuntimeException {
+
+    public QuantityMeasurementException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/app/quantitymeasurement/model/QuantityModel.java
@@ -0,0 +1,19 @@
+package main.java.com.app.quantitymeasurement.model;
+public class QuantityModel {
+
+    private double value;
+    private String unit;
+
+    public QuantityModel(double value, String unit) {
+        this.value = value;
+        this.unit = unit;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+}
